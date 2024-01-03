8aeb6b7ee47406400ec962b60e1434c3f57bae8d
FaridzHidayat <hidayat.faridz18@gmail.com>
2024-01-03 15:26:59 +0700

    on progress

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -39,8 +39,13 @@ export default function AboutUs(){
             <div className="md:grid md:grid-cols-2">                            
               <div className="md:mr-4">
                 <h2 className="font-bold">VISI & MISI</h2>
+                <p className="font-bold">VISI</p>
                 <p className="text-justify">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum nesciunt commodi vel nisi ut alias excepturi ipsum, totam, labore tempora, odit ex iste tempore sed. Fugit voluptatibus perspiciatis assumenda nulla ad nihil, omnis vel, doloremque sit quam autem optio maiores, illum eius facilis et quo consectetur provident dolor similique! Enim voluptatem dicta expedita veritatis repellat dolorum impedit, provident quasi at.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum nesciunt commodi vel nisi ut alias excepturi ipsum.
+                </p>
+                <p className="font-bold">MISI</p>
+                <p className="text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis illum nesciunt commodi vel nisi ut alias excepturi ipsum.
                 </p>
               </div>
               <div className="bg-gray-300">
